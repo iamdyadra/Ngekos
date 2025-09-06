de10d0d8050c8e255951a7add255aaff86ab9c2d
dyadra <dyadra.phindoresi@gmail.com>
2025-09-06 20:48:28 +0700

    Buat Fondasi dan Front-end, Connect Database

--- app/Repositories/CityRepository.php
+++ app/Repositories/CityRepository.php
@@ -2,21 +2,12 @@
 
 namespace App\Repositories;
 
+use App\Repositories\CityRepositoryInterface;
+use App\Models\City;
 class CityRepository implements CityRepositoryInterface
-{
-    /**
-     * Get all cities.
-     *
-     * @return array
-     */
-    public function getAllCities(): array
+{ 
+    public function getAllCities()
     {
-        // This is a placeholder for the actual implementation.
-        // In a real application, this would interact with a database or an external API.
-        return [
-            ['id' => 1, 'name' => 'New York'],
-            ['id' => 2, 'name' => 'Los Angeles'],
-            ['id' => 3, 'name' => 'Chicago'],
-        ];
+    return City::all();
     }
 }
